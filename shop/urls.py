1ed6d6e6f9018fb769e016f930982ac6c7a3f673
« Matteo <matteo.courquin@gmail.com>
2022-12-18 21:49:39 +0100

    :rocket: init project

--- shop/urls.py
+++ shop/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 app_name = 'shop'
 urlpatterns = [
-  path('', views.index, name='index'),
-  path('create/', views.create_view, name='create'),
-  path('<id>/', views.detail_view, name='product'),
+    path('', views.index, name='index'),
+    path('create/', views.create_view, name='create'),
+    path('<id>/', views.detail_view, name='product'),
+    path('<id>/update/', views.update_view, name='update'),
+    path('<id>/delete/', views.delete_view, name='delete'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,6 @@
+from django.shortcuts import (get_object_or_404,
+                              render,
+                              HttpResponseRedirect)
 from .forms import ArticleForm
 from .models import ArticleModel
 from django.shortcuts import render
@@ -30,3 +33,35 @@ def detail_view(request, id):
     context["product_detail"] = ArticleModel.objects.get(id=id)
 
     return render(request, "shop/detail_view.html", context)
+
+
+def update_view(request, id):
+
+    context = {}
+
+    obj = get_object_or_404(ArticleModel, id=id)
+
+    form = ArticleForm(request.POST or None, instance=obj)
+
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect("/shop/"+id)
+
+    context["form"] = form
+
+    return render(request, "shop/update_view.html", context)
+
+
+def delete_view(request, id):
+
+    context = {}
+
+    obj = get_object_or_404(ArticleModel, id=id)
+
+    if request.method == "POST":
+
+        obj.delete()
+
+        return HttpResponseRedirect("/shop/")
+
+    return render(request, "shop/delete_view.html", context)
